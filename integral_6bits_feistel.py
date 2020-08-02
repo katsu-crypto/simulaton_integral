459b5b85c29570c7f4cf49885d156184155b7dcb
katsu <61416317+kkatus03@users.noreply.github.com>
2020-08-03 03:54:40 +0900

    Update integral_6bits_feistel.py

--- integral_6bits_feistel.py
+++ integral_6bits_feistel.py
@@ -1,4 +1,5 @@
 #2020年8月6日発表「Bit-Based Division Propertyの紹介とIntegral・高階差分特性探索への適用」における6bitのFeistel暗号におけるIntegral・高階差分特性探索のシミュレーション
+
 #入力部の特性は　aacccc
 #つまり、上位2ビットで取りうる値すべてが出現し、残り4ビットは固定値である平文集合を用いるIntegral特性
 #高階差分に対応させると、2階差分
